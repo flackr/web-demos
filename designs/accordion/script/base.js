35acd5c4f8de06ddc78bd71969b2e28cd6e8db82
Robert Flack <flackr@gmail.com>
2023-08-24 13:22:06 -0400

    Add beforematch handling to open on find or navigation.

---
+++ designs/accordion/script/base.js
@@ -0,0 +1,25 @@
+
+let initBeforeMatch = function() {
+  let sections = document.querySelectorAll('section > .details');
+  for (let section of sections) {
+    let input = section.parentElement.querySelector('input');
+    section.setAttribute('hidden', 'until-found');
+    // This is problematic because it triggers on every section containing search content.
+    section.addEventListener('beforematch', () => {
+      input.checked = true;
+      input.dispatchEvent(new InputEvent('input'));
+    });
+    input.addEventListener('input', () => {
+      // Run after other input listeners which may modify the checked state.
+      setTimeout(() => {
+        let inputs = document.querySelectorAll('section > label > input');
+        for (let input of inputs) {
+          if (!input.checked) {
+            input.parentElement.parentElement.querySelector('.details').setAttribute('hidden', 'until-found');
+          }
+        }
+      }, 0);
+    });
+  }
+}
+document.addEventListener('DOMContentLoaded', initBeforeMatch);
