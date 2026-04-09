729b2dc1568b14e01a828e5fca7de780fe8b7efa
Robert Flack <flackr@gmail.com>
2026-04-09 10:59:32 -0400

    Add swipe actions in either direction.

--- html/overscrollcontainer/swipe-action/buttons.js
+++ html/overscrollcontainer/swipe-action/buttons.js
@@ -2,17 +2,22 @@ let actions = document.querySelector('#actions');
 
 function addOverscrollArea(container, area) {
   container.appendChild(area);
-  const btn = document.createElement('button');
-  btn.command = 'toggle-overscroll';
-  btn.commandForElement = area;
-  container.appendChild(btn);
-  container.addEventListener('overscrollend', (evt) => {
-    container.remove();
-  });
 }
 
 for (const email of document.querySelectorAll('.email-item')) {
   for (const area of actions.content.children) {
     addOverscrollArea(email, area.cloneNode(true));
   }
+  email.addEventListener('overscrollend', (evt) => {
+    console.log(evt);
+    const command = evt.overscrollTarget.dataset.command;
+    if (command == 'delete')
+      email.remove();
+    else if (command == 'toggle')
+      email.classList.toggle('unread');
+    else
+      throw new Error(`Unsupported command: ${command}`);
+    // Should have a better way to unscroll the overscroll area.
+    email.firstElementChild.scrollIntoView({block: 'nearest', inline: 'nearest'});
+  });
 }
